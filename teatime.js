248f82ed5060f53b2fc8c212d8a2ed8d72de6f2b
herkyl <s.herkyl@gmail.com>
2016-01-18 13:09:20 +0200

    Adding days, hours, minutes, seconds and ms

--- teatime.js
+++ teatime.js
@@ -1,3 +1,10 @@
+var constants = {
+  MS_IN_DAY: 1000 * 60 * 60 * 24,
+  MS_IN_HOUR: 1000 * 60 * 60,
+  MS_IN_MINUTE: 1000 * 60,
+  MS_IN_SECOND: 1000,
+}
+
 var teatime = {
 
   noon: function(d) {
@@ -9,6 +16,32 @@ var teatime = {
     }
     date.setHours(12, 0, 0, 0);
     return date;
+  },
+
+  differenceInDays: function(d1, d2) {
+    var utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+    var utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+    return Math.floor((utc2 - utc1) / constants.MS_IN_DAY);
+  },
+
+  addDays: function (d, days) {
+    return teatime.addMilliseconds(d, days * constants.MS_IN_DAY);
+  },
+
+  addHours: function(d, hours) {
+    return teatime.addMilliseconds(d, hours * constants.MS_IN_HOUR);
+  },
+
+  addMinutes: function(d, minutes) {
+    return teatime.addMilliseconds(d, minutes * constants.MS_IN_MINUTE);
+  },
+
+  addSeconds: function (d, seconds) {
+    return teatime.addMilliseconds(d, seconds * constants.MS_IN_SECOND);
+  },
+
+  addMilliseconds: function (d, ms) {
+    return new Date(d.getTime() + ms);
   }
 
 };
--- test/main.js
+++ test/main.js
@@ -2,16 +2,83 @@ var test = require('tape');
 var teatime = require('../teatime.js');
 
 test('sanity', function (t) {
-  t.equal(100, 100);
+  t.equal(true, true);
   t.end();
 });
 
-test('noon', function (t) {
+test('noon without input date', function (t) {
   var d = teatime.noon();
   t.equal(d.getHours(), 12);
   t.equal(d.getMinutes(), 0);
   t.equal(d.getSeconds(), 0);
   t.equal(d.getMilliseconds(), 0);
   t.equal(d.getDate(), (new Date()).getDate());
+  t.end();
+});
+
+test('noon with input date', function (t) {
+  var date = new Date('Mon Jan 18 2016 13:07:17 GMT+0200 (EET)');
+  var d = teatime.noon(date);
+  t.equal(d.getHours(), 12);
+  t.equal(d.getMinutes(), 0);
+  t.equal(d.getSeconds(), 0);
+  t.equal(d.getMilliseconds(), 0);
+  t.equal(d.getDate(), date.getDate());
+  t.end();
+});
+
+test('adding days', function (t) {
+  var d = teatime.noon(new Date('Mon Jan 18 2016 13:07:17 GMT+0200 (EET)'));
+  t.equal(d.getDate(), 18);
+  t.equal(teatime.addDays(d, 1).getDate(), 19);
+  t.equal(teatime.addDays(d, -1).getDate(), 17);
+  t.end();
+});
+
+test('adding hours', function (t) {
+  var d = teatime.noon();
+  t.equal(teatime.addHours(d, 1).getHours(), 13);
+  t.equal(teatime.addHours(d, -1).getHours(), 11);
+  t.equal(teatime.addHours(d, 24).getHours(), 12);
+  t.equal(teatime.addHours(d, -24).getHours(), 12);
+  t.end();
+});
+
+test('adding minutes', function (t) {
+  var d = teatime.noon();
+  t.equal(teatime.addMinutes(d, 1).getMinutes(), 1);
+  t.equal(teatime.addMinutes(d, -1).getMinutes(), 59);
+  t.end();
+});
+
+test('adding seconds', function (t) {
+  var d = teatime.noon();
+  var added = teatime.addSeconds(d, 1);
+  t.equal(added.getSeconds(), 1);
+  t.equal(added.getMinutes(), 0);
+
+  var subtracted = teatime.addSeconds(d, -1);
+  t.equal(subtracted.getSeconds(), 59);
+  t.equal(subtracted.getMinutes(), 59);
+  t.end();
+});
+
+test('adding milliseconds', function (t) {
+  var d = teatime.noon();
+  var added = teatime.addMilliseconds(d, 1);
+  t.equal(added.getMilliseconds(), 1);
+
+  var subtracted = teatime.addMilliseconds(d, -1);
+  t.equal(subtracted.getMilliseconds(), 999);
+  t.end();
+});
+
+test('adding milliseconds', function (t) {
+  var d = teatime.noon();
+  var added = teatime.addMilliseconds(d, 1);
+  t.equal(added.getMilliseconds(), 1);
+
+  var subtracted = teatime.addMilliseconds(d, -1);
+  t.equal(subtracted.getMilliseconds(), 999);
   t.end();
 });
